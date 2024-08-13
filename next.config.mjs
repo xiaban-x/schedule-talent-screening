f17a408264b9f560d14658ac3f7de088ee216ca4
xiaban <980892894@qq.com>
2024-08-13 09:41:23 +0800

    perf: 修改访问根路径适合的重定向地址

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/talent-screening',
+                destination: '/content-creation',
                 permanent: true,
             },
             {
