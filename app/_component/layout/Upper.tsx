0fbdd78ea1ec659ded69202b0f22f75ad23d6e27
xiaban <980892894@qq.com>
2024-08-17 03:28:01 +0800

    feat: 修改项目顶部路由切换页面逻辑

--- app/_component/layout/Upper.tsx
+++ app/_component/layout/Upper.tsx
@@ -6,33 +6,38 @@ import NotificationIcon from "/public/notification.svg";
 import SettingIcon from "/public/setting.svg";
 import UserIcon from "/public/user.svg";
 import Avatar from "/public/avatar.png";
-import {usePathname} from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
 import Link from "next/link";
+import {Select, Space} from 'antd';
 
 const Upper = () => {
     const pathname = usePathname();
-    const [currentPath, setCurrentPath] = useState(pathname);
+    const router = useRouter();
     const routes = [
-        // {
-        //     name: "达人智能筛选系统",
-        //     path: "talent-screening"
-        // },
-        // {
-        //     name: "广告投放优化系统",
-        //     path: "advertising"
-        // },
         {
-            name: "内容智能创作系统",
-            path: "/content-creation"
+            label: "达人智能筛选系统",
+            value: "/talent-screening"
         },
         {
-            name: "种草营销服务系统",
-            path: "/grass-marketing"
+            label: "广告投放优化系统",
+            value: "/advertising"
+        },
+        {
+            label: "内容智能创作系统",
+            value: "/content-creation"
+        },
+        {
+            label: "种草营销服务系统",
+            value: "/grass-marketing"
         },
     ];
+    const [currentPath, setCurrentPath] =
+        useState(routes.find(item => pathname === item.value)?.label);
+
 
     const handleLinkClick = (path: string) => {
-        setCurrentPath(path);
+        setCurrentPath(routes.find(item => path === item.value)?.label);
+        router.push(`${path}`)
     };
     return (
         <div className="flex w-full justify-between items-center">
@@ -43,24 +48,29 @@ const Upper = () => {
                     有档期么
                 </div>
                 <div className="flex items-center space-x-20px ml-20px">
+
+                    <Select
+                        defaultValue={currentPath}
+                        style={{width: 160}}
+                        onChange={(e) => handleLinkClick(e)}
+                        options={routes}
+                    >
+                        {/*<Link*/}
+                        {/*    href={`/${currentPath.path}`}*/}
+                        {/*    className={`leading-29px whitespace-nowrap px-13px pb-18px border-b-1 border-#ACABAB ${currentPath === route.path ? 'text-primary' : 'text-neutral'}`}*/}
+                        {/*    onClick={() => handleLinkClick(route.path)}*/}
+                        {/*>*/}
+                        {/*    {route.name}*/}
+                        {/*</Link>*/}
+                    </Select>
+
                     {/*{routes.map((route, index) => (*/}
-                    {/*    <div key={index}>*/}
-                    {/*        <Link*/}
-                    {/*            href={`/${route.path}`}*/}
-                    {/*            className={`leading-29px whitespace-nowrap px-13px pb-18px border-b-1 border-#ACABAB ${currentPath === route.path ? 'text-primary' : 'text-neutral'}`}*/}
-                    {/*            onClick={() => handleLinkClick(route.path)}*/}
-                    {/*        >*/}
-                    {/*            {route.name}*/}
-                    {/*        </Link>*/}
+                    {/*    currentPath.includes(route.path) &&*/}
+                    {/*    <div key={index}*/}
+                    {/*         className={'leading-29px whitespace-nowrap px-13px text-18px text-primary fw-700'}>*/}
+                    {/*        {route.name}*/}
                     {/*    </div>*/}
                     {/*))}*/}
-                    {routes.map((route, index) => (
-                        currentPath.includes(route.path) &&
-                        <div key={index}
-                             className={'leading-29px whitespace-nowrap px-13px text-18px text-primary fw-700'}>
-                            {route.name}
-                        </div>
-                    ))}
                 </div>
             </div>
             <div className="flex items-center space-x-[20px]">
