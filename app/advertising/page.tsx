fd47ef4d274982b19a725be4e607eac026fb11e5
xiaban <980892894@qq.com>
2024-08-17 04:51:44 +0800

    feat: 广告投放系统增加pdf展示

--- app/advertising/page.tsx
+++ app/advertising/page.tsx
@@ -1,4 +1,8 @@
+import Image from "next/image";
+
 const page = () => {
-    return <div>advertising page</div>
+    return <div className={"w-full"}>
+        <Image src={'/投放分析报告样例_00.png'} alt={'png'} width={500} height={500} style={{width:'100%',height:'100%'}}/>
+    </div>
 }
 export default page;
