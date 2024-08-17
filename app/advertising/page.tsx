16044e4145ed8b9b6ceb967630d4c10cb1fb1e93
xiaban <980892894@qq.com>
2024-08-18 01:09:11 +0800

    perf: 调整长图清晰度

--- app/advertising/page.tsx
+++ app/advertising/page.tsx
@@ -2,7 +2,18 @@ import Image from "next/image";
 
 const page = () => {
     return <div className={"w-full"}>
-        <Image src={'/投放分析报告样例_00.png'} alt={'png'} width={500} height={500} style={{width:'100%',height:'100%'}}/>
+        <Image
+            src={'/投放分析报告样例_00.png'}
+            alt={'png'}
+            width={1920}
+            height={0}
+            quality={100}
+            style={{
+                width: '100%',
+                height: 'auto', // 确保图片高度自适应
+                objectFit: 'contain' // 保持图片比例
+            }}
+        />
     </div>
 }
 export default page;
